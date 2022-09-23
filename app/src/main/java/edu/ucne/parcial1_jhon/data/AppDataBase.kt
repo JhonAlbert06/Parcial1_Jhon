44ec16a90bbc05d21c4b9774ec61b84afed5887d
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-22 22:22:51 -0400

    Data Base

---
+++ app/src/main/java/edu/ucne/parcial1_jhon/data/AppDataBase.kt
@@ -0,0 +1,18 @@
+package edu.ucne.parcial1_jhon.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+/*
+@Database(
+    entities = [Aux::class],
+    version = 1,
+    exportSchema = false
+)
+
+
+abstract class AppDataBase: RoomDatabase() {
+    abstract val auxDao: AuxDao
+}
+*/
+
