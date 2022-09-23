5ed3465ce8b1f99032fdba9ce41793f44613b01b
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-22 22:35:22 -0400

    Main

--- app/src/main/java/edu/ucne/parcial1_jhon/MainActivity.kt
+++ app/src/main/java/edu/ucne/parcial1_jhon/MainActivity.kt
@@ -13,10 +13,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.parcial1_jhon.ui.aux.EditAuxScreen
 import edu.ucne.parcial1_jhon.ui.aux.HomeAuxScreen
 import edu.ucne.parcial1_jhon.ui.theme.Parcial1_JhonTheme
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
