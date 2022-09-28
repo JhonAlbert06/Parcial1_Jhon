2426ff0e3369284c338ddef7a8aa0f49fdf9f4f7
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-28 18:17:35 -0400

    Data Base

--- app/src/main/java/edu/ucne/parcial1_jhon/data/AppDataBase.kt
+++ app/src/main/java/edu/ucne/parcial1_jhon/data/AppDataBase.kt
@@ -2,17 +2,19 @@ package edu.ucne.parcial1_jhon.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.parcial1_jhon.data.local.dao.ArticuloDao
+import edu.ucne.parcial1_jhon.data.local.entity.Articulo
 
-/*
+
 @Database(
-    entities = [Aux::class],
+    entities = [Articulo::class],
     version = 2,
     exportSchema = false
 )
 
 
 abstract class AppDataBase: RoomDatabase() {
-    abstract val auxDao: AuxDao
+    abstract val articuloDao: ArticuloDao
 }
-*/
+
 
