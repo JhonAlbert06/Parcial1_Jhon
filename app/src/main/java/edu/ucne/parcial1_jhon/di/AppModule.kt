cac22e1f8de6f7a6b035adbf5ac7b427aeeeda97
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-28 18:24:26 -0400

    di AppModule

--- app/src/main/java/edu/ucne/parcial1_jhon/di/AppModule.kt
+++ app/src/main/java/edu/ucne/parcial1_jhon/di/AppModule.kt
@@ -7,20 +7,21 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.parcial1_jhon.data.AppDataBase
 import javax.inject.Singleton
 
-/*
+
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
     @Singleton
     @Provides
-    fun providesDataBase(@ApplicationContext context: Context) : AppDataBase{
+    fun providesDataBase(@ApplicationContext context: Context) : AppDataBase {
         return Room.databaseBuilder(
             context,
             AppDataBase::class.java,
-            "DataBaseName"
+            "AppDataBase"
         ).fallbackToDestructiveMigration().build()
     }
 }
-*/
+
