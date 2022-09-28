aa0107114a0b3b72606b5805de0be959e88dd3fe
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-09-28 18:29:54 -0400

    ViewModel Home(Consulta) Articulos

--- app/src/main/java/edu/ucne/parcial1_jhon/ui/aux/HomeArticuloViewModel.kt
+++ app/src/main/java/edu/ucne/parcial1_jhon/ui/aux/HomeArticuloViewModel.kt
@@ -1,40 +1,44 @@
 package edu.ucne.parcial1_jhon.ui.aux
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import edu.ucne.parcial1_jhon.data.local.entity.Aux
+import edu.ucne.parcial1_jhon.data.local.entity.Articulo
+import edu.ucne.parcial1_jhon.data.repository.ArticuloRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 data class AuxListUiState(
-    val auxs : List<Aux> = emptyList()
+    val articulos : List<Articulo> = emptyList()
 )
 
 @HiltViewModel
-class HomeViewModelAux @Inject constructor(
-    //private val repository: AuxRepository
+class HomeArticuloViewModel @Inject constructor(
+    private val repository: ArticuloRepository
 ): ViewModel() {
 
     private val _uiState = MutableStateFlow(AuxListUiState())
     val uiState : StateFlow<AuxListUiState> = _uiState.asStateFlow()
 
-    /*init {
+    init {
         viewModelScope.launch {
             repository.getAll().collect { list ->
                 _uiState.update {
-                    it.copy(auxs = list)
+                    it.copy(articulos = list)
                 }
             }
         }
     }
 
-    fun delete(aux: Aux) {
+    fun delete(articulo: Articulo) {
         viewModelScope.launch {
-            repository.delete(aux)
+            repository.delete(articulo)
         }
-    }*/
+    }
 
 
 }
